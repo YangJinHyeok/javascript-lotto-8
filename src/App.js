8e377fe5dd4decf70d06f158e096b0efaee9dc70
YangJinHyeok <101380919+YangJinHyeok@users.noreply.github.com>
2025-11-03 22:31:33 +0900

    feat(app): 프로그램 전체 실행 흐름 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,35 @@
+import { Console } from "@woowacourse/mission-utils";
+import LottoInput from "./LottoInput.js";
+import LottoMachine from "./LottoMachine.js";
+import LottoResult from "./LottoResult.js";
+
 class App {
-  async run() {}
+    async run() {
+        try {
+            const input = new LottoInput();
+            const machine = new LottoMachine();
+            const result = new LottoResult();
+
+            const purchaseAmount = await input.askPurchaseAmount();
+            const lottoCount = purchaseAmount / 1000;
+
+            const lottos = machine.generateLottos(lottoCount);
+            input.printPurchasedLottos(lottos);
+
+            const winningNumbers = await input.askWinningNumbers();
+            const bonusNumber = await input.askBonusNumber();
+
+            const resultData = result.calculate(
+                lottos,
+                winningNumbers,
+                bonusNumber
+            );
+            result.print(resultData, lottos.length);
+        } catch (error) {
+            Console.print(error.message);
+            await this.run();
+        }
+    }
 }
 
 export default App;
