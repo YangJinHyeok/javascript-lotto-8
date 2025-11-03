08fff18590f02c8782be27102957d97e107cf2a9
YangJinHyeok <101380919+YangJinHyeok@users.noreply.github.com>
2025-11-03 22:29:49 +0900

    feat(lotto): Lotto 클래스 및 번호 유효성 검증 기능 구현

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,18 +1,28 @@
 class Lotto {
-  #numbers;
+    #numbers;
 
-  constructor(numbers) {
-    this.#validate(numbers);
-    this.#numbers = numbers;
-  }
-
-  #validate(numbers) {
-    if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+    constructor(numbers) {
+        this.#validate(numbers);
+        this.#numbers = numbers;
     }
-  }
 
-  // TODO: 추가 기능 구현
+    #validate(numbers) {
+        if (numbers.length !== 6) {
+            throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+        }
+        const uniqueNumbers = new Set(numbers);
+        if (uniqueNumbers.size !== 6) {
+            throw new Error("[ERROR] 로또 번호는 중복될 수 없습니다.");
+        }
+
+        if ([...numbers].some((num) => num < 1 || num > 45)) {
+            throw new Error("[ERROR] 로또 번호는 1부터 45 사이여야 합니다.");
+        }
+    }
+
+    getNumbers() {
+        return this.#numbers;
+    }
 }
 
 export default Lotto;
